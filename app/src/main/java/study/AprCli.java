7d9192b09b050f042b5cf959bb30d5c5d9e841fd
BJH9 <tfzangin9@gmail.com>
2021-08-04 13:57:07 +0900

    apply cli in apr

--- app/src/main/java/study/AprCli.java
+++ app/src/main/java/study/AprCli.java
@@ -1,8 +1,32 @@
 package study;
 
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.Options;
+
+import edu.handong.csee.lapr.FaultLocalizer;
+import edu.handong.csee.lapr.PatchGenerator;
+import edu.handong.csee.lapr.PatchValidator;
+import edu.handong.csee.lapr.Patcher;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 
 public class AprCli {
+	private String projectDirectoryPath;
+	private String testCaseDirectoryPath;
+	private boolean help;
+	
+	private String projectDirPath;
+	private String testCasePath;
+	
+	private String faultDirectoryPath;
+	private String faultFilePath;
+	private int faultLocationLineNumber;
+	
+	private String[] candidatePatches;
+	private String finalPatch;
 
 	public static void main(String[] args) {
 		AprCli runner = new AprCli();
@@ -12,21 +36,68 @@ public class AprCli {
 	private void run(String[] args) {
 		Options options = createOptions();
 		
-		if()
+		if(parseOption(options, args)) {
+			if(help) {
+				printHelp(options);
+				return;
+			}
+			
+			projectDirPath = projectDirectoryPath;
+			testCasePath = testCaseDirectoryPath;
+			
+			
+		}
+		
+		FaultLocalizer faultLocalizer = new FaultLocalizer();
+		PatchGenerator patchGenerator = new PatchGenerator();
+		PatchValidator patchValidator = new PatchValidator();
+		Patcher patcher = new Patcher();
+		
+		//FaultLocalization: projectDirectoryPath에 테스트케이스를 적용하는 과정을 거쳐 faultDirectoryPath faultFilePath와 faultLocationLineNumber생성한다.
+		faultLocalizer.excute(projectDirPath, testCasePath);
+		faultFilePath = faultLocalizer.getFaultFilePath();
+		faultLocationLineNumber = faultLocalizer.getFaultLocationLineNumber();
+		
+		//PatchGeneration: faultDirectoryPath,  faultFilePath, faultLocationLineNumber위치에 결함에 대한 testcase들을 적용해 패치들을 생성한다. candidate patch
+		candidatePatches = patchGenerator.getPatches(faultDirectoryPath, faultFilePath, faultLocationLineNumber);
+		
+		//PatchValidation: candidate patch에 테스트케이스를 적용해 최종적으로 final patch를 찾는다.
+		finalPatch = patchValidator.getFinalPatches(candidatePatches);
+		
+		//Patch apply: faultFilePath의 오리지널 파일의 결함코드에 correct patch를 적용시켜 최종 수정한다.
+		patcher.fixFaultCode(finalPatch);
+	}
+	
+	private boolean parseOption(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+		
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			
+			projectDirectoryPath = cmd.getOptionValue("f");
+			testCaseDirectoryPath = cmd.getOptionValue("t");
+			help = cmd.hasOption("h");
+		} catch(Exception e) {
+			printHelp(options);
+			return false;
+		}
+		
+		return true;
+		
 		
 	}
 	
 	private Options createOptions() {
 		Options options = new Options();
 		
-		options.addOption(Option.builder("f").longOpt("faultDirPath")
+		options.addOption(Option.builder("f").longOpt("projectDirectoryPath")
 				.desc("set a path of a fault directory")
 				.hasArg()
 				.argName("Path name of fault directory")
 				.required()
 				.build());
 		
-		options.addOption(Option.builder("t").longOpt("testCasePath")
+		options.addOption(Option.builder("t").longOpt("testCaseDirectoryPath")
 				.desc("set a path of a testCasePath")
 				.hasArg()
 				.argName("Path name of test case")
@@ -39,4 +110,12 @@ public class AprCli {
 		
 		return options;
 	}
+	
+	private void printHelp(Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		String header = "APR Test";
+		String footer = "\nPlease report issues";
+		formatter.printHelp("APR", header, options, footer, true);
+	}
+	
 }
